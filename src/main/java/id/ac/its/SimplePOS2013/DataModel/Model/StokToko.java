b49193f18a51e22e1d03a0a748f2d676ea047482
Amanda Tiara Averousi <amandatiara27@gmail.com>
2013-12-14 05:33:55 +0700

    Penambahan atribut utk entitas StokToko, memberikan tipe data yang dibutuhkan, menambahkan annotasi ManyToOne pada sisi StokToko, penambahan setter dan getter method

--- src/main/java/id/ac/its/SimplePOS2013/DataModel/Model/StokToko.java
+++ src/main/java/id/ac/its/SimplePOS2013/DataModel/Model/StokToko.java
@@ -1,5 +1,70 @@
 package id.ac.its.SimplePOS2013.DataModel.Model;
 
-public class StokToko {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="STOK_TOKO")
+public class StokToko {
+	
+	@Id
+	@Column(name="ID_STOK_TOKO")
+	private String idStokToko;
+	
+	@ManyToOne
+	@JoinColumn(name="ID_TOKO")
+	private Toko idToko; 
+	
+	@ManyToOne
+	@JoinColumn(name="ID_BARANG")
+	private Barang idBarang; 
+	
+	@Column(name="STOK_PER_TOKO")
+	private Integer stokPerToko;
+	
+	public StokToko(){
+		
+	}
+	
+	public StokToko(String idStokToko, Integer stokPerToko){
+		this.idStokToko = idStokToko;
+		this.stokPerToko = stokPerToko;
+	}
+	
+	//setter and getter method
+	 public void setIdBarang(Barang idBarang) {
+		this.idBarang = idBarang;
+	}
+	 
+	 public void setIdToko(Toko idToko) {
+		this.idToko = idToko;
+	}
+	 
+	 public void setIdStokToko(String idStokToko) {
+		this.idStokToko = idStokToko;
+	}
+	 
+	 public void setStokPerToko(Integer stokPerToko) {
+		this.stokPerToko = stokPerToko;
+	}
+	  
+	 public Barang getIdBarang() {
+		return idBarang;
+	}
+	 
+	 public Toko getIdToko() {
+		return idToko;
+	}
+	 
+	 public String getIdStokToko() {
+		return idStokToko;
+	}
+	 
+	 public Integer getStokPerToko() {
+		return stokPerToko;
+	}
 }
