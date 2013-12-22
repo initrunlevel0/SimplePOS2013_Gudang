c8c6a3572991525b0c997f678a43cec9eff1b93b
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2013-12-22 15:21:36 +0700

    Added Java template class for BackOffice Controller

---
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangHomeController.java
@@ -0,0 +1,18 @@
+package id.ac.its.SimplePOS2013.Gudang.BackOffice;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GudangHomeController {
+	@RequestMapping(value = "/gudang", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "haha";
+	}
+	
+}
---
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangKelolaBarangToko.java
@@ -0,0 +1,5 @@
+package id.ac.its.SimplePOS2013.Gudang.BackOffice;
+
+public class GudangKelolaBarangToko {
+
+}
