1bee578ecb274ddea16e10fae357311a4e6776fe
m ruslan hafiz <mruslanhafiz@live.co.uk>
2013-12-22 16:12:16 +0700

    menambahkan service PegawaiTokoServiceFO dan StokTokoServiceFO

--- src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceFO/StokTokoServiceFO.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceFO/StokTokoServiceFO.java
@@ -1,5 +1,14 @@
 package id.ac.its.SimplePOS2013.Gudang.ServiceFO;
 
-public interface StokTokoServiceFO {
+import id.ac.its.SimplePOS2013.DataModel.Model.StokToko;
+import id.ac.its.SimplePOS2013.DataModel.Model.Transaksi;
 
+import java.util.List;
+
+public interface StokTokoServiceFO {
+	void tambahStokToko(StokToko stokToko);
+	void suntingStokToko(StokToko stokToko);
+	void hapusStokToko (String idStokToko);
+	StokToko lihatStokToko (String idStokToko);
+	List<StokToko> lihatDaftarStokToko();
 }
--- src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceFO/StokTokoServiceFOImpl.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceFO/StokTokoServiceFOImpl.java
@@ -1,5 +1,40 @@
 package id.ac.its.SimplePOS2013.Gudang.ServiceFO;
 
-public class StokTokoServiceFOImpl implements StokTokoServiceFO {
+import id.ac.its.SimplePOS2013.DataModel.DAO.BaseDAO;
+import id.ac.its.SimplePOS2013.DataModel.Model.PegawaiGudang;
+import id.ac.its.SimplePOS2013.DataModel.Model.StokToko;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StokTokoServiceFOImpl implements StokTokoServiceFO {
+	
+	@Autowired
+	private BaseDAO<StokToko, String> baseDao;
+	
+	@Override
+	public void tambahStokToko(StokToko stokToko){
+		baseDao.add(stokToko);
+	}
+	
+	@Override
+	public void suntingStokToko(StokToko stokToko){
+		baseDao.update(stokToko);
+	}
+	
+	@Override
+	public void hapusStokToko (String idStokToko){
+		baseDao.delete(idStokToko, StokToko.class);
+	}
+	
+	@Override
+	public StokToko lihatStokToko (String idStokToko){
+		return baseDao.view(idStokToko, StokToko.class);
+	}
+	
+	@Override
+	public List<StokToko> lihatDaftarStokToko(){
+		return baseDao.listAll(StokToko.class);
+	}
 }
