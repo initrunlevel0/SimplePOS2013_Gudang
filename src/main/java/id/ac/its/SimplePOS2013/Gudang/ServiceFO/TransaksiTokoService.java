854800529199dc192be2305c5e273f6e4633f6c4
Amanda Tiara Averousi <amandatiara27@gmail.com>
2013-12-22 15:32:15 +0700

    Menambahkan package id.ac.its.SimplePOS2013.Gudang.ServiceFO dan menambahkan TransaksiTokoService dan TransaksiTokoImpl untuk Kasir

---
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceFO/TransaksiTokoService.java
@@ -0,0 +1,12 @@
+package id.ac.its.SimplePOS2013.Gudang.ServiceFO;
+import java.util.List;
+
+import id.ac.its.SimplePOS2013.DataModel.Model.*;
+
+public interface TransaksiTokoService {
+	void tambahTransaksiToko(Transaksi transaksi);
+	void suntingTransaksiToko(Transaksi transaksi);
+	void hapusTransaksiToko (String idTransaksi);
+	Transaksi lihatTransaksiToko (String idTransaksi);
+	List<Transaksi> lihatDaftarTransaksiToko();
+}
