7e3e8e5cb1711840e7f17d785f7bf80d2e07a6c5
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2013-12-23 16:23:34 +0700

    Added some example of Controller+View Implementation

--- src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangHomeController.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangHomeController.java
@@ -1,5 +1,9 @@
 package id.ac.its.SimplePOS2013.Gudang.BackOffice;
 
+import id.ac.its.SimplePOS2013.DataModel.Model.Barang;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
@@ -13,6 +17,18 @@ public class GudangHomeController {
 	@RequestMapping(value = "/gudang", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "tes";
+	}
+	
+	@RequestMapping(value="/gudang/login", method = RequestMethod.GET) 
+	public String loginGet(Model model) {
+		List<Barang> barang = new ArrayList<Barang>();
+		Barang b1 = new Barang();
+		Barang b2 = new Barang();
+		b1.setNamaBarang("Bengbeng");
+		b2.setNamaBarang("Bongbong");
+		barang.add(b1);
+		barang.add(b2);
+		return "login";
 	}
 	
 }
