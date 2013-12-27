3bb117371930a09a8afac938122ec2155fd26b49
Amanda Tiara Averousi <amandatiara27@gmail.com>
2013-12-27 22:04:34 +0700

    menambahkan controller untuk GudangKelolaPegawaiToko

--- src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangKelolaPegawaiToko.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/BackOffice/GudangKelolaPegawaiToko.java
@@ -1,5 +1,41 @@
 package id.ac.its.SimplePOS2013.Gudang.BackOffice;
 
-public class GudangKelolaPegawaiToko {
+import id.ac.its.SimplePOS2013.DataModel.Model.PegawaiToko;
+import id.ac.its.SimplePOS2013.Gudang.ServiceBO.PegawaiTokoService;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GudangKelolaPegawaiToko {
+	
+	@Autowired
+	private PegawaiTokoService PegawaiTokoService;
+	
+	//=============================/list pegawai toko(select table)
+	@RequestMapping(value = "/gudang/pegawai_toko", method = RequestMethod.GET)
+	public String homeKelolaPegawaiToko(Model model) {
+		return "pegawai_toko";
+	}
+	@ModelAttribute("daftarPegawaiToko")
+	public List<PegawaiToko> daftar(){
+		return this.PegawaiTokoService.daftarPegawaiToko();
+	}
+	
+	//==============================tambah pegawaiToko(insert ke database)
+	@RequestMapping(value = "/gudang/pegawai_toko/tambah",method = RequestMethod.GET)
+	public String homeKelolaPegawaiTokoTambah(PegawaiToko pegawaiToko,Model model){
+		return "pegawai_toko_tambah";
+	}
+	//================================sunting pegawaiToko(update table database)
+	@RequestMapping(value = "/gudang/pegawai_toko/sunting",method = RequestMethod.GET)
+	public String homeKelolaPegawaiTokoSunting(PegawaiToko pegawaiToko, Model model){
+		return "pegawai_toko_sunting";
+	}
+	
 }
