7774e120cdf0dc5570bbddf94a28f7beedf6a3a0
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-01-04 19:11:29 +0700

    RESTRUCTURING BEAN: Hibernate Entity Manager bean moved to root-context.xml, component-scan on root-context.xml WORKED: Login on PegawaiGudang

--- src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceBO/PegawaiGudangImplService.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceBO/PegawaiGudangImplService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
@@ -16,9 +17,10 @@ import org.springframework.stereotype.Service;
 import id.ac.its.SimplePOS2013.DataModel.DAO.BaseDAO;
 import id.ac.its.SimplePOS2013.DataModel.Model.PegawaiGudang;
 
-@Service
+@Service(value="pegawaiGudangImplService")
 public class PegawaiGudangImplService implements PegawaiGudangService, UserDetailsService {
 	
+	private static final Logger LOGGER = Logger.getLogger(PegawaiGudangImplService.class.getName());
 	@Autowired
 	private BaseDAO<PegawaiGudang, String> baseDao;
 
@@ -61,7 +63,9 @@ public class PegawaiGudangImplService implements PegawaiGudangService, UserDetai
 		}
 		List<GrantedAuthority> grantedAuthority = new ArrayList<GrantedAuthority>();
 		grantedAuthority.add(new GrantedAuthorityImpl("ROLE_USER"));
+	
 		UserDetails user = new User(dataPegawaiGudang.getIdPegawaiGudang(), dataPegawaiGudang.getPassword(), true, true, true, true,  grantedAuthority);
+		LOGGER.info("Username: " + dataPegawaiGudang.getNamaPegawaiGudang() + ", Password: " + dataPegawaiGudang.getPassword());
 		return user;
 	}
 	
