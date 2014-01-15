93912799b27a44983c403b6e99e035615b67115a
Aisha Yuliandari <aishayuliandari@gmail.com>
2014-01-15 22:55:16 +0700

    membuat tampilan toko, menampilkan daftar toko

--- src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceBO/TokoServiceImpl.java
+++ src/main/java/id/ac/its/SimplePOS2013/Gudang/ServiceBO/TokoServiceImpl.java
@@ -1,5 +1,7 @@
 package id.ac.its.SimplePOS2013.Gudang.ServiceBO;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +25,10 @@ public class TokoServiceImpl implements TokoService{
 		baseDao.update(toko);
 	}
 	
+	@Override
+	public List<Toko> daftarToko(){
+		return baseDao.listAll(Toko.class);
+	}
 	
 	public Toko lihatToko(int idToko){
 		return baseDao.view(idToko, Toko.class);
